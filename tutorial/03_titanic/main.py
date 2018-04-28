dfc819be68d857f432e5214c571f12029657db74
H. Shibata <32999930+drsenri@users.noreply.github.com>
2018-04-28 15:12:08 +0900

    Add files via upload

---
+++ tutorial/03_titanic/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+from myfunc import *
+from sklearn.ensemble import RandomForestClassifier
+
+## 実行ブロック
+
+### train
+train = pd.read_csv("data/train.csv")
+id_train, df_train = process_titanic(train, "train")
+X, y = convert_input(df_train, "Survived")
+# あとでハイパーパラメータの設定を入れる
+model = RandomForestClassifier()
+model.fit(X, y)
+
+### test
+test = pd.read_csv("data/test.csv")
+id_test, df_test = process_titanic(train, "test")
+X_test = convert_input(df_test, None)
+pred = model.predict(X_test)
+result = pd.DataFrame({"PassengerId": "id_test", "Survived": pred})
+result.to_csv("result.csv", index = False)
---
+++ tutorial/03_titanic/myfunc.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+from myfunc import *
+from sklearn.ensemble import RandomForestClassifier
+
+
+def process_titanic(df, type):
+    '''
+    Titanicのデータの前処理を行う
+
+    @param  df    : input data (Data.Frame)
+    @param  type  : process type (str, "train"/"test")
+    @return id    : passenger ID (list)
+    @return df_p  : processed data (Data.Frame)
+
+    データ型と変換の方法
+    @PassengerId : int、除外する
+    @Survived    : int、目的変数として返す
+    @Pclass      : int、
+    @Name        : str
+    @Sex         : str、欠損あり、ダミー変数化して返す
+    @Age         : int、年代別 (10年) でカテゴリ変数化して返す
+    @SibSp       : int
+    @Parch       : int
+    @Ticket      : str、
+    @Fare        : float
+    @Cabin       : 欠損あり
+    @Embarked    : 欠損あり
+    '''
+
+    id = df.PassengerId
+
+    # remove
+    df.drop(["PassengerId", "Name"], axis=1, inplace=True)
+
+    # Sex (to dummy)
+    sex_dummy = pd.get_dummies(df["Sex"])
+
+
+    return id, df_p
+
+
+def convert_input(df, y_name):
+    '''
+    pd.DataFrameからsklearnに入れる形に変換する
+
+    @param  df     : input data (pd.Data.Frame)
+    @param  y_name : column of objective variable (str)
+    @return X      : feature variable (np.ndarray)
+    @return y      : objective variable (np.array)
+    '''
+
+
+    return X, y
